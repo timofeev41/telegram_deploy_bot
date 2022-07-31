8bb47fdbec83273f7fd430b8b0445e250241829c
Nikolas <timofeevnik41@gmail.com>
2022-08-01 02:02:08 +0300

    :zap: Start/Cancel deploy commands

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.dispatcher.filters import Text
 import os
 
-from utils import read_concrete_deployment_schema, read_deployment_schemas_list
+from utils import build_actions_keyboard, read_concrete_deployment_schema, read_deployment_schemas_list
 
 TOKEN = os.getenv("TOKEN")
 if not TOKEN:
@@ -33,21 +33,37 @@ async def send_deploy_info(message: types.Message) -> None:
     """
     This handler will be called when user sends `/deploy` command
     """
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = types.InlineKeyboardMarkup(row_width=4)
     for schema in SCHEMAS:
-        keyboard.add(types.InlineKeyboardButton(text=schema, callback_data=f"deploy_{schema}"))
+        keyboard.add(types.InlineKeyboardButton(text=schema, callback_data=f"info_{schema}"))
     await message.answer("Доступные для деплоя схемы:", reply_markup=keyboard)
 
 
+@dp.callback_query_handler(Text(startswith="info_"))
+async def send_deployment_info(call: types.CallbackQuery):
+    """
+    This handler will catch 'info_*' calls
+    """
+    try:
+        schema = await read_concrete_deployment_schema(call.data[5:])
+        await call.message.edit_text(
+            f"Деплой схемы {schema.name} (Таргет: {schema.target or 'Неизвестно'})\nДетали: {schema.chain} ",
+            reply_markup=await build_actions_keyboard(call.data[5:]),
+        )
+    except FileNotFoundError:
+        await call.message.edit_text(f"Произошла ошибка в обработке схемы {call.data[5:]}")
+
+
 @dp.callback_query_handler(Text(startswith="deploy_"))
-async def sent_deployment_schema(call: types.CallbackQuery):
+async def send_deployment_schema(call: types.CallbackQuery):
     """
     This handler will catch 'deploy_*' calls
     """
-    schema = await read_concrete_deployment_schema(call.data.split("_")[1])
-    await call.message.answer(
-        f"Деплой схемы {schema.name} (Таргет: {schema.target or 'Неизвестно'})\nДетали: {schema.chain} "
-    )
+    call_data = call.data[7:]
+    if call_data.startswith("cancel"):
+        await call.message.edit_text("Деплой схемы отменен")
+    elif call_data.startswith("start"):
+        await call.message.edit_text(f"Деплой схемы {call_data[6:]}")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import json
 import os
 from models import Schema
 from pydantic import parse_obj_as
+from aiogram import types
 
 
 def read_deployment_schemas_list() -> list[str]:
@@ -11,3 +12,10 @@ def read_deployment_schemas_list() -> list[str]:
 async def read_concrete_deployment_schema(schema_file: str) -> Schema:
     schema = open("deployment_schemas/" + schema_file, mode="r", encoding="utf-8")
     return parse_obj_as(Schema, json.load(schema))
+
+
+async def build_actions_keyboard(schema_file: str) -> types.InlineKeyboardMarkup:
+    keyboard = types.InlineKeyboardMarkup(row_width=4)
+    keyboard.add(types.InlineKeyboardButton(text="Деплой", callback_data=f"deploy_start_{schema_file}"))
+    keyboard.add(types.InlineKeyboardButton(text="Отменить", callback_data=f"deploy_cancel_{schema_file}"))
+    return keyboard
